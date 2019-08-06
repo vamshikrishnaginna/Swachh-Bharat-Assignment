e4e28ce5321cf610a08e1b33f9971170f7497de7
G Vamshi Krishna <vamshiginna1606@gmail.com>
2019-08-06 10:53:42 +0530

    Auto stash before rebase of "origin/master"

---
+++ src/main/java/com/rubiconred/swachhbharat/Register.java
@@ -0,0 +1,16 @@
+package com.rubiconred.swachhbharat;
+
+import java.util.Scanner;
+
+public class Register {
+    private String uName;
+    private String uEmail;
+    private String uPhoneNumber;
+
+    Scanner sc=new Scanner(System.in);
+
+
+
+
+
+}
