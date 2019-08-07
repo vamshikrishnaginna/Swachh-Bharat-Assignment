2eceb9cec27356b2716c419e633cd6b07912dc72
G Vamshi Krishna <vamshiginna1606@gmail.com>
2019-08-07 23:17:54 +0530

    16.ReportDemo class is created to replace previous Report class for better visualization

--- src/main/java/com/rubiconred/swachhbharat/App.java
+++ src/main/java/com/rubiconred/swachhbharat/App.java
@@ -6,72 +6,14 @@ package com.rubiconred.swachhbharat;
 public class App {
     public static void main(String[] args) {
 
-//        User newUser = new User();
-//        Wastage uWastage = new Wastage();
-//        Report report = new Report(newUser, uWastage);
+        User newUser = new User();
+        Wastage uWastage = new Wastage();
+        ReportDemo report = new ReportDemo(newUser, uWastage);
 
 //        XStream xs = new XStream();
 //        WeightsAndPoints wap=new WeightsAndPoints();
 //        String points=xs.toXML(wap.getPointsTable()) ;
 //        System.out.println(points);
-        int[] productId = new int[]
-                {1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010,};
-        String[] productName = new String[]{"Pepsi", "kissan jam", "Herbaloil", "Garnier man's", "Lays chips", "biscuits", "Bournvita", "Cadbury", "Parker Vector", "Nescafe",};
-        String[] productType = new String[]{"Cold Drink", "Jam", "Oil", "Face wash", "chips", "Biscuits", "Health Supplement", "Chocolate", "Stationary", "Coffee",};
-        float[] productPrice = new float[]{24, 65, 30, 79, 10, 20, 140, 20, 150, 80,};
-
-
-        int productNameMaxlength = 0;
-        int productTypeMaxlength = 0;
-
-
-        for (String productName1 : productName) {
-            if (productNameMaxlength < productName1.length()) {
-                productNameMaxlength = productName1.length();
-            }
-        }
-
-
-        for (String productType1 : productType) {
-            if (productTypeMaxlength < productType1.length()) {
-                productTypeMaxlength = productType1.length();
-            }
-        }
-
-
-        for (int i = 0; i < productType.length; i++) {
-            System.out.print(i);
-
-            System.out.print("\t");
-
-            System.out.print(productId[i]);
-
-            System.out.print("\t");
-
-            System.out.print(productName[i]);
-
-            for (int j = 0; j <= productNameMaxlength - productName[i].length
-                    (); j++) {
-
-                System.out.print(" ");
-
-            }
-
-            System.out.print("\t");
-
-            System.out.print(productType[i]);
-
-            for (int j = 0; j <= productTypeMaxlength - productType[i].length
-                    (); j++) {
-                System.out.print(" ");
-            }
-
-            System.out.print("\t");
-
-            System.out.println(productPrice[i]);
-        }
-
-
 
 
 
--- src/main/java/com/rubiconred/swachhbharat/ReportDemo.java
+++ src/main/java/com/rubiconred/swachhbharat/ReportDemo.java
@@ -1,4 +1,49 @@
 package com.rubiconred.swachhbharat;
 
 public class ReportDemo {
+    private int count = 0;
+    private int typeMaxlength = 0;
+    private int brandMaxlength = 0;
+
+    public ReportDemo(User user, Wastage wastage) {
+        count = wastage.wasteTypeItems.size();
+        System.out.println("             REPORT           ");
+        System.out.println("=================+++++++=============");
+        System.out.println("User:" + user.getuName());
+        count = wastage.getTotalItems();
+        System.out.println("Total Items:" + count);
+        System.out.println("Item Details:");
+        for (String type1 : wastage.getWasteTypeItems()) {
+            if (typeMaxlength < type1.length()) {
+                typeMaxlength = type1.length();
+            }
+        }
+        for (String brand1 : wastage.getBrandItems()) {
+            if (brandMaxlength < brand1.length()) {
+                brandMaxlength = brand1.length();
+            }
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(i);
+
+            System.out.print("\t");
+
+            System.out.print(wastage.getWasteTypeItems().get(i));
+
+            for (int j = 0; j <= typeMaxlength - wastage.getWasteTypeItems().get(i).length(); j++) {
+
+                System.out.print(" ");
+
+            }
+
+            System.out.print("\t");
+            System.out.println(wastage.getBrandItems().get(i));
+
+
+        }
+
+
+    }
+
 }
