b2ad3b42c1a25d92750bf2e6d0806ab6357a42d4
Vamshi Krishna <vamshiginna1606@gmail.com>
2019-08-10 10:15:05 +0530

    33. App Java handles all the programs in the project

--- src/main/java/com/rubiconred/swachhbharat/App.java
+++ src/main/java/com/rubiconred/swachhbharat/App.java
@@ -1,24 +1,27 @@
 package com.rubiconred.swachhbharat;
 
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
-
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws Exception {
 
         User newUser = new User();
         WastageDispose uWastageDispose = new WastageDispose();
-        ReportDemo report = new ReportDemo(newUser, uWastageDispose);
+        ReportGenerator report = new ReportGenerator(newUser, uWastageDispose, false);
 
-        //XStream xs = new XStream();
-        //WeightsAndPoints wap=new WeightsAndPoints();
-        // String points=xs.toXML(wap.getPointsTable()) ;
-        //System.out.println(points);
-        ReportFetch rf = new ReportFetch();
-        rf.printValues();
+//        Gson gson=new Gson();
+//        String report1=gson.toJson(report);
+//        System.out.println(report1);
+//        //XStream xs = new XStream();
+//        //WeightsAndPoints wap=new WeightsAndPoints();
+//        // String points=xs.toXML(wap.getPointsTable()) ;
+//        //System.out.println(points);
+//        ReportFetch rf = new ReportFetch();
+//        rf.printValues();
+//        WeightsAndPoints weights = new WeightsAndPoints("plastic", "pepsi");
+//        System.out.println(weights.getWeight());
+
+
     }
 }
