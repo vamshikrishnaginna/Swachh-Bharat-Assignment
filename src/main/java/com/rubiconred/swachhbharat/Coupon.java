b1185b654465d7e356f04785873a4ee8f1bd4888
Vamshi Krishna <vamshiginna1606@gmail.com>
2019-08-10 10:17:08 +0530

    34. The coupon generates the coupon code in the end.

--- src/main/java/com/rubiconred/swachhbharat/Coupon.java
+++ src/main/java/com/rubiconred/swachhbharat/Coupon.java
@@ -4,17 +4,42 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class Coupon {
+    User user;
+    private String couponCode;
 
-    public String createRandomCode(int codeLength) {
+    public Coupon(User user) {
+        this.user = user;
+        couponCode = createRandomCode();
+    }
+
+
+    public String createRandomCode() {
         char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
         StringBuilder sb = new StringBuilder();
         Random random = new SecureRandom();
-        for (int i = 0; i < codeLength; i++) {
+        for (int i = 0; i < 8; i++) {
             char c = chars[random.nextInt(chars.length)];
             sb.append(c);
         }
         String output = sb.toString();
 
         return output;
+    }
+
+    public User getUser() {
+
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
     }
 }
